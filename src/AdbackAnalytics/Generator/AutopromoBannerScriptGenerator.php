8b4762471c583319ac8dabc93d20f34dc738e166
Nicolas Thal <nico.th4l@gmail.com>
2017-05-18 16:27:39 +0200

    do not set banner id in tag

--- src/AdbackAnalytics/Generator/AutopromoBannerScriptGenerator.php
+++ src/AdbackAnalytics/Generator/AutopromoBannerScriptGenerator.php
@@ -2,41 +2,29 @@
 
 namespace Dekalee\AdbackAnalytics\Generator;
 
-use Dekalee\AdbackAnalytics\Exception\AutopromoBannerIdMissing;
-
 /**
  * Class AutopromoBannerScriptGenerator
  */
 class AutopromoBannerScriptGenerator extends AbstractScriptGenerator implements ScriptGeneratorInterface
 {
     /**
-     * @param int $id
-     *
      * @return string
-     *
-     * @throws AutopromoBannerIdMissing
      */
-    public function generate($id = null)
+    public function generate()
     {
-        if(is_null($id)) {
-            throw new AutopromoBannerIdMissing();
-        }
-
         if (!$this->cache->isAutopromoBannerConfigured()) {
             return '';
         }
 
         $url = $this->cache->getAutopromoBannerUrl();
         $script = $this->cache->getAutopromoBannerScript();
-        $encodedId = base64_encode($id);
 
         $script = <<<EOS
-(function (a,d){var s,t,cs,ds,dd;s=d.createElement('script');cs=d.currentScript;
-    ds=d.createElement('span');ds.id=Math.random().toString(36).substring(7);
-    dd=cs.parentNode.insertBefore(ds,cs);
-    s.src=a;s.async=1;s.setAttribute('data-name',ds.id);s.setAttribute('data-id', '$encodedId');
-    t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);})
-("https://$url/$script.js", document);
+(function (a,d){var s,t;s=d.createElement('script');
+    s.src=a;s.async=1;
+    t=d.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s,t);
+})("https://$url/$script.js", document);
 EOS;
 
         return $script;
